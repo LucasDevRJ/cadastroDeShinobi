7632f67725f87920767e8a93d5637166a2bfa382
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-12 21:08:50 -0300

    Criação do projeto.

---
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/CadastroDeShinobiApplication.java
@@ -0,0 +1,13 @@
+package com.github.lucasdevrj.cadastrodeshinobi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroDeShinobiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroDeShinobiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/lucasdevrj/cadastrodeshinobi/CadastroDeShinobiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasdevrj.cadastrodeshinobi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDeShinobiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
