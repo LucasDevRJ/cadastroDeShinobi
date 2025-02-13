e91f8264a7a34d21815b493dd3891d7d192d293d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-13 19:19:25 -0300

    Organização do projeto via pacotes, criação da classe MissaoModel e relacionamento entre Shinobi e Missão

--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/shinobi/ShinobiController.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/shinobi/ShinobiController.java
@@ -1,4 +1,4 @@
-package com.github.lucasdevrj.cadastrodeshinobi;
+package com.github.lucasdevrj.cadastrodeshinobi.shinobi;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
