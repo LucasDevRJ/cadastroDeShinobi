d82ca2a38ec3134a6770d6359a4c705dcaf2d3fd
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-16 16:56:21 -0300

    Revert "Loombok"
    This reverts commit ee1cca08a6585e0d5a3bb628793aefaa5cbeda05.

--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoModel.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoModel.java
@@ -2,17 +2,11 @@ package com.github.lucasdevrj.cadastrodeshinobi.missao;
 
 import com.github.lucasdevrj.cadastrodeshinobi.shinobi.ShinobiModel;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_missoes")
-@NoArgsConstructor
-@AllArgsConstructor
-@Data
 public class MissaoModel {
 
     @Id
@@ -23,4 +17,43 @@ public class MissaoModel {
     private String rank;
     @OneToMany(mappedBy = "missao") //uma missão pode ser feita por muitos ninjas
     private List<ShinobiModel> shinobis;
+
+    public MissaoModel() {
+
+    }
+
+    public MissaoModel(String nome, String rank) {
+        this.nome = nome;
+        this.rank = rank;
+    }
+
+    public MissaoModel(String nome, String descricao, String rank) {
+        this.nome = nome;
+        this.descricao = descricao;
+        this.rank = rank;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
 }
