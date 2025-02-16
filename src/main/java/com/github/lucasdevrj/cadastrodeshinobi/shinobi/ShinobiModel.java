ee1cca08a6585e0d5a3bb628793aefaa5cbeda05
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-16 13:47:13 -0300

    Loombok

--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/shinobi/ShinobiModel.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/shinobi/ShinobiModel.java
@@ -2,9 +2,15 @@ package com.github.lucasdevrj.cadastrodeshinobi.shinobi;
 
 import com.github.lucasdevrj.cadastrodeshinobi.missao.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity //transforma um classe em uma entidade no Banco de Dados
 @Table(name = "tb_shinobis") //Criar a tabela com o nome definido
+@NoArgsConstructor //cria construtor sem argumentos
+@AllArgsConstructor //cria construtor com todos argumentos
+@Data //criar getters e setters
 public class ShinobiModel {
 
     @Id //para gerar ID's na entidade
@@ -16,38 +22,4 @@ public class ShinobiModel {
     @ManyToOne //muitos ninjas podem fazer uma missão
     @JoinColumn(name = "missoes_id") //chave estrangeira
     private MissaoModel missao;
-
-    public ShinobiModel() {
-
-    }
-
-    public ShinobiModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
