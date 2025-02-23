78a9b51befae754f247ecea825e9ca1a56f7918d
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-02-23 12:05:35 -0300

    feat: criando métodos para fazer requisições CRUD nas Missões.

--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoController.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoController.java
@@ -1,15 +1,38 @@
 package com.github.lucasdevrj.cadastrodeshinobi.missao;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController //indica para a classe que ela é uma Controladora para a API
-@RequestMapping//rota da qual acessaremos a API
+@RequestMapping("missoes")//rota da qual acessaremos a API
 public class MissaoController {
 
-    @GetMapping("/missao") //pegar informação passada pelo método
-    public String missao() {
-        return "Sua missão é rank S!";
+    //POST - Enviar requisição para criar uma Missão
+    @PostMapping("/criar")
+    public String criarMissao() {
+        return "Missão criada com sucesso!";
+    }
+
+    //GET - Enviar requisição para listar Missões
+    @GetMapping("/listar")
+    public String exibirMissoes() {
+        return "Missões listadas!";
+    }
+
+    //GET - Enviar requisição para exibir Missão por ID
+    @GetMapping("/exibirPorID")
+    public String exibirMissaoPorID() {
+        return "Missão exibida com sucesso!";
+    }
+
+    //PUT - Enviar requisição para atualizar Missão por ID
+    @PutMapping("/atualizar")
+    public String atualizarMissao() {
+        return "Missão atualizada com sucesso!";
+    }
+
+    //DELETE - Enviar requisição para excluir Missão por ID
+    @DeleteMapping("/excluir")
+    public String excluirMissao() {
+        return "Missão deletada com sucesso!";
     }
 }
