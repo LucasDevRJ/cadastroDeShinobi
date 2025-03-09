593cfd9460f1ff1fb0fd933c1be38c257ef487a0
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-03-09 14:09:40 -0300

    feat: adição da função para listar Missões.

--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoController.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoController.java
@@ -2,9 +2,17 @@ package com.github.lucasdevrj.cadastrodeshinobi.missao;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController //indica para a classe que ela é uma Controladora para a API
 @RequestMapping("missoes")//rota da qual acessaremos a API
 public class MissaoController {
+
+    private MissaoService missaoService;
+
+    public MissaoController(MissaoService missaoService) {
+        this.missaoService = missaoService;
+    }
 
     //POST - Enviar requisição para criar uma Missão
     @PostMapping("/adicionar")
@@ -14,8 +22,8 @@ public class MissaoController {
 
     //GET - Enviar requisição para listar Missões
     @GetMapping("/listar")
-    public String exibirMissoes() {
-        return "Missões listadas!";
+    public List<MissaoModel> listarMissoes() {
+        return missaoService.listar();
     }
 
     //GET - Enviar requisição para exibir Missão por ID
--- src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoService.java
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/missao/MissaoService.java
@@ -1,4 +1,21 @@
 package com.github.lucasdevrj.cadastrodeshinobi.missao;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Service //indica que a classe é um Service, o qual tem os métodos da lógica da API
 public class MissaoService {
+
+    private MissaoRepository missaoRepository;
+
+    public MissaoService(MissaoRepository missaoRepository) {
+        this.missaoRepository = missaoRepository;
+    }
+
+    //lógica para listar as missões
+    public List<MissaoModel> listar() {
+        return missaoRepository.findAll();
+    }
 }
