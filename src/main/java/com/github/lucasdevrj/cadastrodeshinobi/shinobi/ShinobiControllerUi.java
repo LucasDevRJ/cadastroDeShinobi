4ecac6252ac04f91c3f6d616aa52b8afb965b040
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2025-03-15 17:42:16 -0300

    refactor: adição de página HTML e do Model, para assim termos interface gráfica.

---
+++ src/main/java/com/github/lucasdevrj/cadastrodeshinobi/shinobi/ShinobiControllerUi.java
@@ -0,0 +1,27 @@
+package com.github.lucasdevrj.cadastrodeshinobi.shinobi;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("shinobis/ui")
+public class ShinobiControllerUi {
+
+    private final ShinobiService shinobiService;
+
+    public ShinobiControllerUi(ShinobiService shinobiService) {
+        this.shinobiService = shinobiService;
+    }
+
+    @GetMapping("/listar")
+    public String listarShinobis(Model model) { //biblioteca com interfaces gráficas
+        List<ShinobiDTO> shinobis = shinobiService.listarShinobis();
+        model.addAttribute("shinobis", shinobis);
+        return "Shinobis listados";
+    }
+}
